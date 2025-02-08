01aa0a0bbf2ae04d205bd26e91adbab80e36e01b
gurau-anil <gurau.anil@gmail.com>
2025-02-08 11:49:34 -0700

    testing Token based authentication

---
+++ AuthenticationTestApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using AuthenticationTestApi.Models;
+using AuthServiceLibrary.Entities;
+using AuthServiceLibrary.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationTestApi.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConfigurationSection _jwtSettings;
+        private readonly IAuthService _authService;
+        public AuthenticationController(IConfiguration configuration, IAuthService authService)
+        {
+            _configuration = configuration;
+            _jwtSettings = _configuration.GetSection("JwtSettings");
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginModel model)
+        {
+
+            var result = await _authService.LoginAsync(new LoginRequest { Username = model.UserName , Password = model.Password});
+            if (result.Succeeded) {
+                return Ok(new { Token = result.Token, ExpiresAt = result.ExpiresAt });
+            }
+
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access");
+            return Unauthorized(ModelState);
+        }
+    }
+}
--- AuthenticationTestApi/Program.cs
+++ AuthenticationTestApi/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using AuthServiceLibrary;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +12,41 @@ builder.Services.AddJwtAuthentication(connectionString, options =>
     options.Issuer = "Test";
     options.Audience = "Test";
     options.ExpirationMinutes = 10;
-    options.SecretKey = builder.Configuration.GetValue<string>("JwtSettings:SecretKey");
+    options.SecretKey = builder.Configuration.GetValue<string>("JwtSettings:SecretKey")??String.Empty;
 });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("api-docs", new OpenApiInfo { Title = "JWT API", Version = "1.0" });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' followed by JWT token. Example : Bearer {token}"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -25,7 +54,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/api-docs/swagger.json", "JWT API 1.0"));
 }
 
 app.UseHttpsRedirection();

dd797a97819f14e76d5d87b111a85f67c90c54c6
gurau-anil <gurau.anil@gmail.com>
2025-07-25 12:48:14 -0700

    Auth Constants

---
+++ AuthServiceLibrary/AuthConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServiceLibrary
+{
+    public static class AuthConstants
+    {
+        public const string AccessToken = "access-token";
+        public const string RefreshToken = "refresh-token";
+    }
+}

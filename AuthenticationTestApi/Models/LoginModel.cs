22e497676f8cac2d111a2ec4568f20473a281d99
gurau-anil <gurau.anil@gmail.com>
2025-08-24 20:12:34 -0700

    Fluent validation on login and register model

--- AuthenticationTestApi/Models/LoginModel.cs
+++ AuthenticationTestApi/Models/LoginModel.cs
@@ -1,8 +1,22 @@
-﻿namespace AuthenticationTestApi.Models
+﻿using FluentValidation;
+
+namespace AuthenticationTestApi.Models
 {
     public class LoginModel
     {
         public string UserName { get; set; }
         public string Password { get; set; }
     }
+
+    public class LoginModelValidator : AbstractValidator<LoginModel>
+    {
+        public LoginModelValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+        }
+
+    }
+
 }
--- AuthenticationTestApi/Models/RegisterModel.cs
+++ AuthenticationTestApi/Models/RegisterModel.cs
@@ -15,15 +15,27 @@ namespace AuthenticationTestApi.Models
     {
         public RegisterModelValidator()
         {
-            RuleFor(x => x.Username).NotNull();
-            RuleFor(user => user.Password)
-                .NotEmpty().WithMessage("Password cannot be empty.")
-                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
-                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
-                .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
-                .Matches("[0-9]").WithMessage("Password must contain at least one number.")
-                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
-            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Username is required");
+
+            RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid.");
+            });
+
+            RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Password)
+                    .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                    .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                    .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                    .Matches("[0-9]").WithMessage("Password must contain at least one number.")
+                    .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+            });
         }
+
     }
 }

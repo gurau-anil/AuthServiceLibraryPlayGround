03d7edce8d1d1c043a1ab36ea4d13c47ab613fd4
gurau-anil <gurau.anil@gmail.com>
2025-09-18 13:48:48 -0700

    email confirmation on user register

--- AuthenticationTestApi/Controllers/UserManagementController.cs
+++ AuthenticationTestApi/Controllers/UserManagementController.cs
@@ -2,9 +2,12 @@
 using AuthServiceLibrary.Models;
 using AuthServiceLibrary.Services.Interfaces;
 using AutoMapper;
+using EmailService.Model;
+using EmailService.Services.Interfaces;
 using FluentValidation;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
+using Scriban;
 
 namespace AuthenticationTestApi.Controllers
 {
@@ -14,12 +17,18 @@ namespace AuthenticationTestApi.Controllers
         private readonly IUserManagementService _userManagement;
         private readonly IMapper _mapper;
         private readonly IValidator<RegisterModel> _validator;
+        private readonly IConfiguration _config;
+        private readonly IEmailService _emailService;
+        private readonly IHostEnvironment _env;
 
-        public UserManagementController(IUserManagementService userManagement, IMapper mapper, IValidator<RegisterModel> validator)
+        public UserManagementController(IUserManagementService userManagement, IMapper mapper, IValidator<RegisterModel> validator, IConfiguration config, IEmailService emailService, IHostEnvironment env)
         {
             _userManagement = userManagement;
             _mapper = mapper;
             _validator = validator;
+            _config = config;
+            _emailService = emailService;
+            _env = env;
         }
 
         [HttpPost]
@@ -28,7 +37,21 @@ namespace AuthenticationTestApi.Controllers
         {
             await ValidateModelAsync(model, hasMultipleError: true);
             var result = await _userManagement.RegisterUser(_mapper.Map<UserRegisterModel>(model));
-            return Ok($"User : {model.Username} registered to the system");            
+
+            var emailConfirmationLink = $"{_config.GetValue<string>("ClientUrl")}/auth/confirm-email?email={model.Email}&token={result.Token}";
+            var emailBody = await GetEmailConfirmationEmailBody(new { EmailConfirmationLink = emailConfirmationLink });
+
+            BackgroundJob.Enqueue<IEmailService>(emailService => emailService.SendAsync(new EmailSendModel
+            {
+                RecipientEmail = model.Email,
+                Subject = "Email Confirmation",
+                Body = emailBody
+            }));
+
+            return Ok(@$"
+                User : {model.Username} registered to the system, 
+                and confirmation token has been sent to the email address: {model.Email}. 
+                Confirm your Email before LoggingIn.");
         }
 
         [HttpDelete]
@@ -36,8 +59,15 @@ namespace AuthenticationTestApi.Controllers
         public async Task<IActionResult> DeleteAsync(string username)
         {
             UserModel user = await _userManagement.GetByUsernameAsync(username);
-            bool result = await _userManagement.DeleteByUsernameAsync(username);
+            await _userManagement.DeleteByUsernameAsync(username);
             return Ok($"User: {username} removed from the system");
+        }
+
+        private async Task<string> GetEmailConfirmationEmailBody(object model)
+        {
+            string emailTemplate = await System.IO.File.ReadAllTextAsync(Path.Combine(_env.ContentRootPath, "wwwroot", $"files/{Constants.EmailConfirmationTemplateFileName}"));
+            var template = Template.Parse(emailTemplate);
+            return template.Render(model);
         }
     }
 }

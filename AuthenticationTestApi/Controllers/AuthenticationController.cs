988bb77042dbce2a8dec23584c1789d32cb987ce
gurau-anil <gurau.anil@gmail.com>
2025-09-18 13:48:23 -0700

    email confirmation and password reset

--- AuthenticationTestApi/Controllers/AuthenticationController.cs
+++ AuthenticationTestApi/Controllers/AuthenticationController.cs
@@ -2,10 +2,14 @@
 using AuthServiceLibrary;
 using AuthServiceLibrary.Models;
 using AuthServiceLibrary.Services.Interfaces;
+using EmailService.Model;
+using EmailService.Services.Interfaces;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
-using System.Net;
+using Scriban;
 using System.Text;
+using static Org.BouncyCastle.Math.EC.ECCurve;
 
 namespace AuthenticationTestApi.Controllers
 {
@@ -15,12 +19,15 @@ namespace AuthenticationTestApi.Controllers
         private readonly IConfiguration _configuration;
         private readonly IConfigurationSection _jwtSettings;
         private readonly IAuthService _authService;
-        public AuthenticationController(IConfiguration configuration, 
-            IAuthService authService)
+        private readonly IHostEnvironment _env;
+        public AuthenticationController(IConfiguration configuration,
+            IAuthService authService,
+            IHostEnvironment env)
         {
             _configuration = configuration;
             _jwtSettings = _configuration.GetSection("JwtSettings");
             _authService = authService;
+            _env = env;
         }
 
         [HttpPost]
@@ -34,11 +41,31 @@ namespace AuthenticationTestApi.Controllers
         }
 
         [HttpGet]
-        [Route("confirm-email/{userId}/{token}")]
-        public async Task<IActionResult> ConfirmEmailToken(string userId, string token)
+        [Route("confirm-email")]
+        public async Task<IActionResult> ConfirmEmailToken(string email, string token)
         {
-            await _authService.ValidateEmailToken(userId, token);
+            await _authService.ValidateEmailToken(email, Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(token)));
             return Ok("Email Validated Successfully");
+        }
+
+        [HttpPost]
+        [Route("send-email-confirmation")]
+        public async Task<IActionResult> SendConfirmEmailToken(string email)
+        {
+            var token = await _authService.GenerateEmailConfirmationTokenAsync(email);
+            var encodedToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+            var emailConfirmationLink = $"{_configuration.GetValue<string>("ClientUrl")}/auth/confirm-email?email={email}&token={encodedToken}";
+            var emailBody = await GetEmailConfirmationEmailBody(new { EmailConfirmationLink = emailConfirmationLink });
+
+            BackgroundJob.Enqueue<IEmailService>(emailService => emailService.SendAsync(new EmailSendModel
+            {
+                RecipientEmail = email,
+                Subject = "Email Confirmation",
+                Body = emailBody
+            }));
+
+            return Ok($"Email Confirmation link has been sent to email: {email}");
         }
 
 
@@ -56,24 +83,36 @@ namespace AuthenticationTestApi.Controllers
         {
             var token = await _authService.GeneratePasswordResetTokenAsync(email);
             var encodedToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
-            return Ok($"{_configuration.GetValue<string>("ClientUrl")}/auth/reset-password?token={encodedToken}&email={email}");
+
+            var passwordResetLink = $"{_configuration.GetValue<string>("ClientUrl")}/auth/reset-password?token={encodedToken}&email={email}";
+            var emailBody = await GetPasswordResetEmailBody(new { PasswordResetLink = passwordResetLink });
+
+            BackgroundJob.Enqueue<IEmailService>(emailService => emailService.SendAsync(new EmailSendModel
+            {
+                RecipientEmail = email,
+                Subject = "Reset your password.",
+                Body = emailBody
+            }));
+
+            return Ok($"Password reset link has been sent to email: {email}");
         }
 
         [HttpPost]
         [Route("reset-password")]
         public async Task<IActionResult> ResetPasswordAsync(ResetPasswordModel model)
         {
+            await ValidateModelAsync(model, hasMultipleError: true);
             var token = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(model.Token));
 
-            var result = await _authService.ResetPasswordAsync(model.Email, model.Password, token);
-            return result? Ok("Password has been Reset."): StatusCode((int)HttpStatusCode.InternalServerError, "Password Reset Failed.");
+            await _authService.ResetPasswordAsync(model.Email, model.Password, token);
+            return Ok("Password reset successful.");
         }
 
         private void IssueAccessTokenCookie(HttpContext context, AuthResult result)
         {
             context.Response.Cookies.Append(AuthConstants.AccessToken, result.Token, new CookieOptions
             {
-                Domain = "localhost",
+                Domain = Request.Host.Host,
                 Path = "/",
                 HttpOnly = true,
                 Secure = true,
@@ -87,7 +126,7 @@ namespace AuthenticationTestApi.Controllers
         {
             context.Response.Cookies.Append(AuthConstants.AccessToken, "", new CookieOptions
             {
-                Domain = "localhost",
+                Domain = Request.Host.Host,
                 Path = "/",
                 HttpOnly = true,
                 Secure = true,
@@ -95,6 +134,20 @@ namespace AuthenticationTestApi.Controllers
                 SameSite = SameSiteMode.None,
                 Expires = DateTime.Now.AddDays(-1)
             });
+        }
+
+        private async Task<string> GetPasswordResetEmailBody(object model)
+        {
+            string emailTemplate = await System.IO.File.ReadAllTextAsync(Path.Combine(_env.ContentRootPath, "wwwroot", $"files/{Constants.PasswordResetTemplateFileName}"));
+            var template = Template.Parse(emailTemplate);
+            return template.Render(model);
+        }
+
+        private async Task<string> GetEmailConfirmationEmailBody(object model)
+        {
+            string emailTemplate = await System.IO.File.ReadAllTextAsync(Path.Combine(_env.ContentRootPath, "wwwroot", $"files/{Constants.EmailConfirmationTemplateFileName}"));
+            var template = Template.Parse(emailTemplate);
+            return template.Render(model);
         }
     }
 }

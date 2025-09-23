65768d18802003021085c76a80dd5e428e959b9f
gurau-anil <gurau.anil@gmail.com>
2025-09-22 22:46:38 -0700

    mergefields helper

---
+++ AuthenticationTestApi/Helpers/MergeFieldHelper.cs
@@ -0,0 +1,13 @@
+﻿using AuthenticationTestApi.Models.MergeField;
+using Scriban;
+
+namespace AuthenticationTestApi.Helpers
+{
+    public static class MergeFieldHelper
+    {
+        public static string PopulateMergeFields(string content, object model)
+        {
+            return Template.Parse(content).Render(model);
+        }
+    }
+}

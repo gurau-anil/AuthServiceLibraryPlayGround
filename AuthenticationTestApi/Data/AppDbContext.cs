c2550009ac6f77ef634c845d3f3821114bc3d0b9
gurau-anil <gurau.anil@gmail.com>
2025-09-22 22:45:13 -0700

    Data context and migration

---
+++ AuthenticationTestApi/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AuthenticationTestApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationTestApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<EmailTemplate> EmailTemplates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmailTemplate>(entity =>
+            {
+                entity.ToTable("EmailTemplates");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Subject).HasMaxLength(100).IsRequired(true);
+                entity.Property(e => e.Body).IsRequired(true);
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

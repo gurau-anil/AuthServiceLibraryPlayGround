df0b724c384fab7b3ebeea722461cb079efb75d0
gurau-anil <gurau.anil@gmail.com>
2025-10-06 23:57:25 -0700

    usermodel update

--- AuthServiceLibrary/Models/UserModel.cs
+++ AuthServiceLibrary/Models/UserModel.cs
@@ -4,6 +4,11 @@
     {
         public Guid Id { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name { 
+            get => $"{FirstName} {LastName}";
+        }
         public string Email { get; set; }
         public DateTime? LastLogin { get; set; }
     }

ed022cd21494a2c28c71cf60162e105f5b7e72b5
gurau-anil <gurau.anil@gmail.com>
2025-10-08 23:30:49 -0700

    new migrations

---
+++ AuthenticationTestApi/Migrations/20251007224037_New Tables added.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenticationTestApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTablesadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

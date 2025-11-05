c2fbdbbaf5675507c2f86f08148d220b4cbff75e
gurau-anil <gurau.anil@gmail.com>
2025-11-04 20:31:36 -0700

    Pagination helper

---
+++ AuthenticationTestApi/Helpers/PaginationHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace AuthenticationTestApi.Helpers
+{
+
+    public class PagedResult<T>
+    {
+
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public IEnumerable<T> Items { get; set; }
+    }
+
+    public static class PaginationHelper
+    {
+        public static PagedResult<T> GetPaged<T>(
+            this IQueryable<T> query, int page, int pageSize)
+        {
+            var totalCount = query.Count();
+            var items = query.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedResult<T>
+            {
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalCount = totalCount,
+                Items = items
+            };
+        }
+    }
+}
